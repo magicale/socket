e49c563571686c629d7be195cb49c62e1838d9a8
cheny.le <lele_cheny@126.com>
2013-08-09 16:41:16 +0800

    add socket test file
    client can send msg to server

---
+++ client.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define MAXLINE 4096
+#define SOCK_SERVER_PATH "/home/cheny.le/cheny/socket_server"
+#define SOCK_CLIENT_PATH "/home/cheny.le/cheny/socket_client"
+
+void main(void)
+{
+    int client_fd;
+    struct sockaddr_un server_addr, client_addr;
+    socklen_t server_addr_len;
+    char buf_send[MAXLINE];
+    char buf_recv[MAXLINE]
+
+    //unlink no use socket
+    if(access(SOCK_CLIENT_PATH, R_OK) == 0) {
+        printf("socket file already exits,unlink it!\n");
+        unlink(SOCK_CLIENT_PATH);
+    }
+
+    //create socket
+    if((client_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket fail");
+        exit(1);
+    }
+    printf("socket ok!\n");
+
+    //bind socket
+    memset(&client_addr, 0x00, sizeof(struct sockaddr_un));
+    client_addr.sun_family = AF_UNIX;
+    strncpy(client_addr.sun_path, SOCK_CLIENT_PATH, (sizeof(client_addr.sun_path) - 1));
+    if(bind(client_fd, (struct sockaddr *)&client_addr, sizeof(struct sockaddr_un)) == -1) {
+        close(client_fd);
+        perror("bind fail!");
+        exit(1);
+    }
+    printf("bind ok!\n");
+
+    //connect socket
+    memset(&server_addr, 0x00, sizeof(struct sockaddr_un));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCK_SERVER_PATH, (sizeof(server_addr.sun_path) - 1));
+    server_addr_len = sizeof(struct sockaddr_un);
+    if(connect(client_fd, (struct sockaddr *)&server_addr, server_addr_len) != 0) {
+        close(client_fd);
+        perror("connect fail");
+        exit(1);
+    }
+    printf("connect ok!\n");
+
+    do {
+        //send buf
+        memset(buf, 0x00, sizeof(buf));
+        printf("Input:");
+        //scanf("%s", buf);
+        //here is a bug, buf may overflow
+        if(gets(buf) == NULL) {
+            perror("gets fail");
+        }
+        if(send(client_fd, buf, strlen(buf), 0) != strlen(buf)) {
+            perror("send fail");
+        }
+    } while(strcmp(buf, "quit") != 0);
+
+    close(client_fd);
+    printf("close ok!\n");
+}
---
+++ server.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+
+#define SOCK_SERVER_PATH "/home/cheny.le/cheny/socket_server"
+#define SOCK_CLIENT_PATH "/home/cheny.le/cheny/socket_client"
+#define LISTEN_BACKLOG 10
+#define MAXLINE 4096
+
+void main(void)
+{
+    int server_fd, client_fd;
+    struct sockaddr_un server_addr;
+    struct sockaddr_un client_addr;
+    socklen_t client_addr_len;
+    char buf_recv[MAXLINE];
+    int num;
+
+    //unlink no use socket
+    if(access(SOCK_SERVER_PATH, R_OK) == 0) {
+        printf("socket file already exist,unlink it!\n");
+        unlink(SOCK_SERVER_PATH);
+    }
+
+    //create socket
+    if((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket fail!");
+        exit(1);
+    }
+    printf("socket ok!\n");
+
+    //bind socket
+    memset(&server_addr, 0x00, sizeof(struct sockaddr_un));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCK_SERVER_PATH, (sizeof(server_addr.sun_path) - 1));
+
+    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) == -1) {
+        close(server_fd);
+        perror("bind fail!");
+        exit(1);
+    }
+    printf("bind ok!\n");
+
+    //listen socket
+    if(listen(server_fd, LISTEN_BACKLOG) == -1) {
+        close(server_fd);
+        perror("listen fail!");
+        exit(1);
+    }
+    printf("listen ok!\n");
+
+    //accept socket
+    memset(&client_addr, 0x00, sizeof(struct sockaddr_un));
+    client_addr.sun_family = AF_UNIX;
+    strncpy(client_addr.sun_path, SOCK_CLIENT_PATH, (sizeof(client_addr.sun_path) - 1));
+    client_addr_len = sizeof(struct sockaddr_un);
+    if((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
+        close(client_fd);
+        close(server_fd);
+        perror("accept fail!");
+        exit(1);
+    }
+    printf("accept ok!\n");
+
+    do {
+        //recv buf
+        memset(buf_recv, 0x00, sizeof(buf_recv));
+        num = recv(client_fd, buf_recv, MAXLINE, 0);
+        printf("Output:%s\n", buf_recv);
+    } while((strcmp(buf_recv, "quit") != 0) && num > 0);
+
+    close(client_fd);
+    close(server_fd);
+    printf("close ok!\n");
+}
